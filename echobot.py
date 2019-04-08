13e3e1e9c2b0244131e4a4eea7b0390dbf0274cf
Halum <7rat13@gmail.com>
2019-04-08 14:37:38 +0530

    Init.

---
+++ echobot.py
@@ -0,0 +1,15 @@
+import telebot
+
+bot = telebot.TeleBot("863992561:AAFzD5yt1v_5u7Fx5PXnvm1YKJi7TyIHhJI")
+
+@bot.message_handler(commands= ['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hello, how are you?')
+
+
+@bot.message_handler(func= lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.polling()
